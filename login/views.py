5a5dfd546f8fdfc71ce139a7ba3fce3f2a95e297
Bruno M <declic62gmail.com>
2023-09-07 09:36:41 +0200

    feat: authentication with personal form

--- login/views.py
+++ login/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, get_user_model
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 
+from .models import CustomUser
 
-# Create your views here.
+
 def signin(request):
     form = AuthenticationForm
     if request.method == "POST":
@@ -17,18 +18,25 @@ def signin(request):
         else:
             messages.info(request, "login ou mot de passe incorrect !")
             return redirect("login:connection")
+
     return render(request, "login_views/login.html", context={"form": form})
 
 
 def signup(request):
-    form = UserCreationForm(request.POST)
     if request.method == "POST":
-        if form.is_valid():
-            form.save()
-            return redirect("profile")
-    else:
-        form = UserCreationForm()
-    return render(request, 'login_views/signup.html', context={"form": form})
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+        if password != password2:
+            return render(request, "login_views/signup.html", context={'error': "Nom d'utilisateur ou mot de passe incorrect"})
+        user = CustomUser.objects.create_user(username=username, password=password, email=email)
+        login(request, user)
+        return redirect("login:profile")
+    return render(request, 'login_views/signup.html')
+
+
+
 
 
 def disconnect(request):
